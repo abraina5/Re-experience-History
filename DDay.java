3ca45fdfd12c010cbc9ddb85c32ab235d0ec76f2
Abhay Raina <75186151+abraina5@users.noreply.github.com>
2026-05-28 10:57:21 -0400

    Add GUI elements for Allies and Nazi options

--- DDay.java
+++ DDay.java
@@ -3,8 +3,36 @@ import javax.swing.*;
 public class DDay {
     public static void intro() {
         JFrame f= new JFrame();
+        
+        JLabel label1 = new JLabel("Would you like to see the Allies side or the Nazi side?");
+        
+        label1.setBounds(50,30,380,10);
+
+        JButton b1 = new JButton("Allies");
+        b1.setBounds(50, 100, 180, 40);
+        b1.addActionListener(n -> DDay.allies());
+        
+        
+        JButton b2 = new JButton("Nazi");
+        b2.setBounds(260, 100, 180, 40);
+        b2.addActionListener(n -> DDay.nazi());
+        
+        
+        f.add(b1);
+        f.add(b2);
+        f.add(label1);
+
+        
         f.setSize(500, 400);
         f.setLayout(null);
         f.setVisible(true);
+    }
+    
+    public static void allies() {
+        
+    }
+    
+    public static void nazi() {
+        
     }
 }
