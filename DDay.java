392dbdbed318bbeab68727e0f99da0b1de63f780
Abhay Raina <75186151+abraina5@users.noreply.github.com>
2026-05-28 10:45:09 -0400

    Create DDay class with intro method for JFrame

---
+++ DDay.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class DDay {
+    public static void intro() {
+        JFrame f= new JFrame();
+        f.setSize(500, 400);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+}
